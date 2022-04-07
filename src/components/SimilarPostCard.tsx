bebf35881ea484021150b5c977725450a727b6af
Onuorah Ike <ikecodes@gmail.com>
2022-04-07 12:24:34 +0100

    fixed media query issues

--- src/components/SimilarPostCard.tsx
+++ src/components/SimilarPostCard.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { BsCalendarDate } from 'react-icons/bs';
 import styled from 'styled-components';
 import BgImage from '../assets/images/chairman.jpg';
-import Image from '../shared/Image';
 const SimilarPostCard = () => {
   return (
     <div className='bg-light my-3'>
       <Container className='row py-2'>
-        <div className='col-lg-5'>
-          <Image src={BgImage} h={5} unit='rem' alt='similar' />
+        <div className='col-lg-5 '>
+          <Image src={BgImage} alt='similar' />
         </div>
-        <div className='col-lg-7 p-0 text-dark'>
-          <h6 className='fw-bold'>
+        <div className='col-lg-7 text-dark'>
+          <h6 className='fw-bold mt-2'>
             How to Create a Productive Work-From-Home Office
           </h6>
           <div className='d-flex align-items-center'>
@@ -30,5 +29,15 @@ const Container = styled.div`
     font-size: 0.8rem;
   }
 `;
-
+const Image = styled.img`
+  height: 5rem;
+  width: 100%;
+  object-fit: cover;
+  @media (max-width: 768px) {
+    width: 30%;
+  }
+  @media (max-width: 576px) {
+    width: 40%;
+  }
+`;
 export default SimilarPostCard;
